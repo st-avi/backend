215cf090c58224b7c3da6e37ae4a3524dfaeef8b
Smallten <smallten@smallten.me>
2026-01-05 13:12:00 +0800

    chore: change db to PostgreSQL

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "backend/internal/packed"
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
-	_ "github.com/gogf/gf/contrib/nosql/redis/v2"
-
 	"backend/internal/cmd"
+	_ "backend/internal/packed"
+
+	_ "github.com/gogf/gf/contrib/drivers/pgsql/v2"
+	_ "github.com/gogf/gf/contrib/nosql/redis/v2"
 	"github.com/gogf/gf/v2/os/gctx"
 )
 
